80582d3cbaeadeb08319bbd6dc3df557156a7a9f
Jasoliya Manoj <48120921+Manojj999@users.noreply.github.com>
2020-06-28 19:35:36 +0530

    HomePage Completed

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./HomePage-style.scss";
+import Directory from "../../Components/directory/Directory";
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory />
+    </div>
+  );
+};
+export default HomePage;
