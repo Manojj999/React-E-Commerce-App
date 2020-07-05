4c5e3aeaaa1c375a95af9b04d45d2f10702cb52d
Jasoliya Manoj <48120921+Manojj999@users.noreply.github.com>
2020-07-05 09:37:19 +0530

    update menu with Router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import React from 'react';
+
 import './App.css';
 import HomePage from './pages/HomePage/HomePage';
+import { Switch, Route } from 'react-router-dom';
 
+const HatsPage = () => {
+  return (<div>
+    <h1>Hats Page</h1>
+  </div>)
+}
 function App() {
   return (
     <div>
-      <HomePage />
- 
+      {/* <HomePage /> */}
+      <Switch>
+
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
+
     </div>
   );
 }
