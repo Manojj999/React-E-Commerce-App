5e55291b67367d13853807f33ca7cdafea4c4f83
Jasoliya Manoj <48120921+Manojj999@users.noreply.github.com>
2020-07-28 13:32:06 +0530

    added Local Storage with redux persistance

--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import store from './redux/Store';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store,persistor} from './redux/Store';
+
 
 ReactDOM.render(
   <Provider store={store}>
     <BrowserRouter>
+    <PersistGate persistor={persistor}>
+
       <App />
+    </PersistGate>
     </BrowserRouter>
   </Provider>,
   document.getElementById('root')
--- src/redux/root-reducer.jsx
+++ src/redux/root-reducer.jsx
@@ -1,10 +1,19 @@
 import {combineReducers} from 'redux';
 import UserReducer from './user/UserReducer';
 import CardReducer from './cart/CartReducer';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
+const persistConfig = {
+    key:'root',
+    storage,
+    whitelist:['cart']
+}
 
-export default combineReducers({
+const rootReducer =combineReducers({
     user:UserReducer,
     cart:CardReducer,
 
 })
+
+export default persistReducer(persistConfig,rootReducer)
